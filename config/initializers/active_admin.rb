0ef589aef3a470f2a780a70493c279c955cf5b33
railosmith <railosmith@garage.(none)>
2012-05-02 21:38:55 +0530

    set the correct path for js

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -6,6 +6,7 @@ ActiveAdmin.setup do |config|
   # for each of the active admin pages.
   #
   config.site_title = "Real Good Food"
+config.register_javascript 'ckeditor/init.js'
 
   # Set the link url for the title. For example, to take
   # users to your main site. Defaults to no link.
